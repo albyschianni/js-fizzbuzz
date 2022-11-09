8d086a99fe17baa55e1388193171609452cdf8ec
albyschianni <alberto.schianni@gmail.com>
2022-11-09 17:33:09 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// programma che stampi numeri da 1 a 100, quando divisibili per 3 "Fizz", per 5 "Buzz", per entrambi "FizzBuzz"
+
+// ciclo stampa numeri da 1 a 100
+
+for (let i = 1; i <= 100; i++){
+    
+
+    if (i % 3 === 0){
+
+        let Fizz = "Fizz";
+        console.log(Fizz);
+
+    } even (i % 5 === 0){
+
+        let Buzz = "Buzz";
+        console.log(Buzz);
+
+    } even (i % 5 === 0 && i % 3 === 0 ){
+
+        let FizzBuzz = "FizzBuzz"
+        console.log(FizzBuzz);
+
+    } else if (){
+        console.log("interazione", i)
+    }
+}
+
+
+// condizione divisibilità numeri 
+
