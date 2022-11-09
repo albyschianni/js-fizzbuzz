0d518a90ef24af9b13fcb4a0fc3cf1eb59b0b623
albyschianni <alberto.schianni@gmail.com>
2022-11-09 22:42:20 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,26 +2,40 @@
 
 // ciclo stampa numeri da 1 a 100
 
+const contenitore = document.querySelector(".container");
+
 for (let i = 1; i <= 100; i++){
     
+    const element = document.createElement("div");
 
-    if (i % 3 === 0){
+    element.classList.add("box");
+    contenitore.append(element);
 
-        let Fizz = "Fizz";
-        console.log(Fizz);
-
-    } even (i % 5 === 0){
-
-        let Buzz = "Buzz";
-        console.log(Buzz);
-
-    } even (i % 5 === 0 && i % 3 === 0 ){
+    if ((i % 5 === 0) && (i % 3 === 0)){
 
         let FizzBuzz = "FizzBuzz"
         console.log(FizzBuzz);
+        element.append(FizzBuzz);
+        element.style.backgroundColor="red"
 
-    } else if (){
+    } else if (i % 5 === 0){
+
+        let Buzz = "Buzz";
+        console.log(Buzz);
+        element.append(Buzz);
+        element.style.backgroundColor="yellow"
+
+    } else if (i % 3 === 0){
+
+        let Fizz = "Fizz";
+        console.log(Fizz);
+        element.append(Fizz);
+        element.style.backgroundColor="green"
+        
+
+    } else {
         console.log("interazione", i)
+        element.append(i);
     }
 }
 
